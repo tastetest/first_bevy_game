c11f902b71002a0308e14abd55c4b7f7715b385c
tastetest <tastytothesupreme@gmail.com>
2022-08-23 22:18:57 -0600

    fixed translations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use bevy::prelude::*;
+
 fn main() {
     App::new()
+        .add_startup_system(spawn_square)
+        .add_system(square_move)
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+#[derive(Component)]
+struct Square;
+
+fn spawn_square(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let entity_spawn = Vec3::ZERO;
     commands.spawn_bundle(Camera2dBundle::default());
     commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("pictures/icon.png"),
+        texture: asset_server.load("./icon.png"),
+        transform: Transform::from_translation(entity_spawn),
         ..default()
-    });
-}  
+    })
+    .insert(Square);
+
+}
+
+fn square_move(mut query: Query<&mut Transform, With<Square>>) {
+        for mut transform in query.iter_mut() {
+            transform.translation += 1.2;
+        }
+}
