fc74de3b84a1b95cf92f607605d814d580a69d49
tastetest <tastytothesupreme@gmail.com>
2022-08-26 17:12:44 -0600

    improved sprite, added scaling and debugging tools

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use bevy::prelude::*;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 
 fn main() {
     App::new()
-        .add_startup_system(spawn_square)
         .add_startup_system(background_tiles)
+        .add_startup_system(spawn_square)
         .add_system(square_move)
         .add_system(bevy::window::close_on_esc)
         .add_plugins(DefaultPlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .run();
 }
 
@@ -25,13 +28,15 @@ fn spawn_square(mut commands: Commands, asset_server: Res<AssetServer>) {
     let entity_spawn = Vec3::ZERO;
     commands.spawn_bundle(Camera2dBundle::default());
     commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("./icon.png"),
-        transform: Transform::from_translation(entity_spawn),
+        texture: asset_server.load("./character.png"),
+        transform: Transform::from_translation(entity_spawn)
+            .with_scale(Vec3::splat(3.0)),
         ..default()
     })
     .insert(Square);
 
 }
+
 fn square_move(mut query: Query<&mut Transform, With<Square>>, keys: Res<Input<KeyCode>>) {
     if keys.pressed(KeyCode::W) {
         for mut transform in query.iter_mut() {
