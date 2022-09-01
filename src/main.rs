4c490f6c50a636bfcd1774b698a829442401c1e5
tastetest <tastytothesupreme@gmail.com>
2022-08-31 22:49:23 -0600

    added in ldtk

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,36 @@
 use bevy::prelude::*;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy_ecs_ldtk::prelude::*;
 
 mod player;
 
 fn main() {
     App::new()
-        .add_startup_system(player::spawn_player)
-        .add_system(player::player_movement)
-        .add_system(enemy_spawn)
-        .add_system(bevy::window::close_on_esc)
+        .add_plugin(LdtkPlugin)
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_map)
+        .add_startup_system(player::spawn_player)
+        .insert_resource(LevelSelection::Index(0))
+        .add_system(player::player_movement)
+        .add_system(bevy::window::close_on_esc)
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .run();
 }
 
+fn setup_map(mut commands: Commands, asset_server: Res<AssetServer>) {
 
-fn background_tiles(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("./background_dirt.png"),
-        ..default()
-    });
+        commands.spawn_bundle(Camera2dBundle::default());
+        asset_server.watch_for_changes().unwrap();
+
+        let ldtk_handle = asset_server.load("./Typical_TopDown_example.ldtk");
+        let map_entity = commands.spawn().id();
+
+        commands.entity(map_entity).insert_bundle(LdtkWorldBundle {
+            ldtk_handle,
+            ..Default::default()
+        });
+
 }
 
-fn enemy_spawn(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let enemy_init_coords = Vec3::ZERO;
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("./enemy.png"),
-        transform: Transform::from_translation(enemy_init_coords)
-            .with_scale(Vec3::splat(2.0)),
-        ..default()
 
-    });
-}
